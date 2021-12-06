f8aa083b72719d43fa8f46f5185490337b8b6102
adlighozian <adlighozian1@gmail.com>
2021-12-06 17:22:01 +0700

    by adlighozian

--- script.js
+++ script.js
@@ -9,15 +9,33 @@ var typed = new Typed(".typing-1", {
 // navbar scroll
 $(document).ready(function() {
     $(window).scroll(function() {
-        if (this.scrollY > 20) {
+        if (this.scrollY > 1) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
-        if (this.scrollY > 30) {
+        if (this.scrollY > 1) {
             $('.scroll-up-btn').addClass("show");
         } else {
             $('.scroll-up-btn').removeClass("show");
         }
     })
 });
+
+//slide-up script
+$('.scroll-up-btn').click(function() {
+    $('html').animate({ scrollTop: 0 });
+});
+
+//active navbar
+const li = document.querySelectorAll(".links");
+const sec = document.querySelectorAll("section");
+
+function activeMenu() {
+    let len = sec.length;
+    while (--len && window.scrollY < sec[len].offsetTop) {}
+    li.forEach(ltx => ltx.classList.remove("active"));
+    li[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll", activeMenu);
